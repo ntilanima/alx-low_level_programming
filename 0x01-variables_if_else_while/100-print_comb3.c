2651f7e3f6a1280541d595e422124fba2ae1b93a
Ntilanima <michaelamabiaaudi@gmail.com>
2023-06-16 12:56:08 -0700

    Task of 0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,10 +11,10 @@ int main(void)
 int digit1 = 0, digit2;
 while (digit1 <= 9)
 {
-digit2 =0;
+digit2 = 0;
 while (digit2 <= 9)
 {
-if (digit1 != digit2 && digit1< digit2)
+if (digit1 != digit2 && digit1 < digit2)
 {
 putchar(digit1 + 48);
 putchar(digit2 + 48);
