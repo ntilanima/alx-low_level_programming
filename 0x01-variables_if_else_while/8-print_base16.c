19e5f6d040bb49d1e4bb8690d5457d2c7241e3b1
Ntilanima <michaelamabiaaudi@gmail.com>
2023-06-16 11:35:05 -0700

    Task of 0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -prints all the numbers of base 16 in lowercase,
+ * Return: 0
+ */
+
+int main(void)
+{
+int n;
+char letter;
+for (n = 0; n <= 10; n++)
+putchar((n % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
+
