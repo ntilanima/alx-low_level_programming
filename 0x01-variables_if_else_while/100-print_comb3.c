ad8cf9deb40885b62be44bc12005f7a9df37aa95
Ntilanima <michaelamabiaaudi@gmail.com>
2023-06-16 12:09:40 -0700

    Task of 0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,12 +14,13 @@ for (digit1 = 0; digit1 < 10; digit1++)
 {
 for (digit2 = 0; digit2 < 10; digit2++)
 {
+if (digit1 != digit2)
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 
 if (digit1 == 9 && digit2 == 9)
 continue;
-			
+
 putchar(',');
 putchar(' ');
 }
