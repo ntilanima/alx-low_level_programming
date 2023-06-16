f834980185523375599b95a8a9324af94a4b4d10
Ntilanima <michaelamabiaaudi@gmail.com>
2023-06-16 12:31:40 -0700

    Task of 0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,9 @@ int main(void)
 {
 int digit1, digit2;
 
-for (digit1 = 0; digit1 < 10; digit1++)
+for (digit1 = 0; digit1 < 10; digit1 != digit2; digit1++)
 {
-for (digit2 = 0; digit2 < 10; digit2++)
-{
-for (digit1 != digit2 && digit2 != digit1)
+for (digit2 = 0; digit2 < 10; digit2 != digit1; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
@@ -24,7 +22,6 @@ continue;
 
 putchar(',');
 putchar(' ');
-}
 }
 }
 
