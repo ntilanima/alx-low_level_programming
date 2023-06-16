fdaae5f0a56b35b9969ed27235fccd938e833560
Ntilanima <michaelamabiaaudi@gmail.com>
2023-06-16 11:01:38 -0700

    Task of 0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  main - prints all single digit of base 10 starting from 0
+ *  Return: 0
+ */
+
+int main(void)
+{
+int n;
+for (n = 0; n < 0; n++)
+printf("%d", n);
+printf('/');
+return (0)
+}
